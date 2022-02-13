135159fd757ca607784839ea79de1ca18ce4d185
noname <48761044+noname0310@users.noreply.github.com>
2022-02-13 12:12:49 +0900

    Update Box2dGameBootstrapper.ts

--- src/asset/Box2dGameBootstrapper.ts
+++ src/asset/Box2dGameBootstrapper.ts
@@ -2,24 +2,37 @@ import {
     Bootstrapper,
     Camera,
     CssSpriteRenderer,
-    SceneBuilder
+    EditorGridRenderer,
+    GameObject,
+    PlayerGridMovementController,
+    PrefabRef,
+    SceneBuilder,
+    TrackCameraController
 } from "the-world-engine";
 import { Vector3 } from "three";
-import { Rotator } from "./script/Rotator";
 
 export class Box2dGameBootstrapper extends Bootstrapper {
     public run(): SceneBuilder {
         const instantiater = this.engine.instantiater;
+
+        const cursor = new PrefabRef<GameObject>();
         
         return this.sceneBuilder
-            .withChild(instantiater.buildGameObject("camera", new Vector3(0, 0, 10))
+
+            .withChild(instantiater.buildGameObject("cursor")
+                .withComponent(CssSpriteRenderer)
+                .withComponent(PlayerGridMovementController)
+                .getGameObject(cursor))
+            
+            
+            .withChild(instantiater.buildGameObject("camera", new Vector3(0, 0, 100))
                 .withComponent(Camera, c => {
                     c.viewSize = 100;
-                }))
-
-            .withChild(instantiater.buildGameObject("test_object")
-                .withComponent(CssSpriteRenderer)
-                .withComponent(Rotator))
+                })
+                .withComponent(TrackCameraController, c => {
+                    c.setTrackTarget(cursor.ref!);
+                })
+                .withComponent(EditorGridRenderer))
         ;
     }
 }
