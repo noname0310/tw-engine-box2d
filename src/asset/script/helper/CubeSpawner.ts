637e2093fe5ab29cd011b00ecf1e67969f400adc
noname <48761044+noname0310@users.noreply.github.com>
2022-02-16 15:00:31 +0900

    move spawner

--- src/asset/script/helper/CubeSpawner.ts
+++ src/asset/script/helper/CubeSpawner.ts
@@ -1,8 +1,10 @@
 import { Component, CssHtmlElementRenderer } from "the-world-engine";
-import { Vector2, Vector3 } from "three";
-import { BoxCollider2D } from "./collider/BoxCollider2D";
-import { PhysicsProcessor } from "./PhysicsProcessor";
-import { RigidbodyType2D, RigidBody2D } from "./RigidBody2D";
+import { Quaternion, Vector2, Vector3 } from "three";
+import { BoxCollider2D } from "../collider/BoxCollider2D";
+import { PhysicsProcessor } from "../PhysicsProcessor";
+import { RigidbodyType2D, RigidBody2D, CollisionDetectionMode2D } from "../RigidBody2D";
+import { AddForceOnce } from "./AddForceOnce";
+import { AddTorqueOnce } from "./AddTorqueOnce";
 
 export class CubeSpawner extends Component {
     private _physicsProcessor: PhysicsProcessor|null = null;
@@ -20,7 +22,8 @@ export class CubeSpawner extends Component {
             this.engine.scene.addChildFromBuilder(
                 this.engine.instantiater.buildGameObject(
                     "cube",
-                    new Vector3(this.transform.position.x, this.transform.position.y, 0)
+                    new Vector3(this.transform.position.x, this.transform.position.y, 0),
+                    new Quaternion()//.setFromAxisAngle(new Vector3(0, 0, 1), Math.PI / 4)
                 )
                     .withComponent(CssHtmlElementRenderer, c => {
                         const div = document.createElement("div");
@@ -34,11 +37,14 @@ export class CubeSpawner extends Component {
                     .withComponent(RigidBody2D, c => {
                         c.physicsProcessor = this._physicsProcessor!;
                         c.bodyType = RigidbodyType2D.Dynamic;
+                        c.collisionDetection = CollisionDetectionMode2D.Discrete;
                         c.mass = 1;
                     })
                     .withComponent(BoxCollider2D, c => {
                         c.size = new Vector2(10, 10);
                     })
+                    .withComponent(AddForceOnce, c => c.enabled = false)
+                    .withComponent(AddTorqueOnce, c => c.enabled = false)
             );
         }
     };
