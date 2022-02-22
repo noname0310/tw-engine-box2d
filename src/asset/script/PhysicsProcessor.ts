78514282ded3e0aff4859c7476ca1f55a5bf2591
noname <48761044+noname0310@users.noreply.github.com>
2022-02-22 18:01:29 +0900

    contect listener test

--- src/asset/script/PhysicsProcessor.ts
+++ src/asset/script/PhysicsProcessor.ts
@@ -1,5 +1,6 @@
 import { Component } from "the-world-engine";
 import * as b2 from "../../box2d.ts/build/index";
+import { RigidBody2D } from "./RigidBody2D";
 
 export class PhysicsProcessor extends Component {
     public static readonly unitScalar = 0.1;
@@ -8,7 +9,27 @@ export class PhysicsProcessor extends Component {
 
     private _world: b2.World = new b2.World(new b2.Vec2(0, -9.81));
 
-    public update() {
+    public awake(): void {
+        this._world.SetContactListener(
+            new class extends b2.ContactListener {
+                public override BeginContact(contact: b2.Contact<b2.Shape, b2.Shape>): void {
+                    console.log(
+                        "BeginContact",
+                        (contact.GetFixtureA().GetBody().GetUserData() as RigidBody2D).gameObject.name,
+                        (contact.GetFixtureB().GetBody().GetUserData() as RigidBody2D).gameObject.name);
+                }
+
+                public override EndContact(contact: b2.Contact<b2.Shape, b2.Shape>): void {
+                    console.log(
+                        "EndContact",
+                        (contact.GetFixtureA().GetBody().GetUserData() as RigidBody2D).gameObject.name,
+                        (contact.GetFixtureB().GetBody().GetUserData() as RigidBody2D).gameObject.name);
+                }
+            }
+        );
+    }
+
+    public update(): void {
         this._world!.Step(
             this.engine.time.deltaTime,
             PhysicsProcessor._velocityIterations,
